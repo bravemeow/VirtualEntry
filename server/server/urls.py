f16abc6047c1e296ff544fc94909aca966772e58
Brave Meow <Bravemeow5@gmail.com>
2021-03-19 16:27:24 +0900

    Connect MariaDB

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
 from . import views
 
 app_name = 'backend'
 urlpatterns = [
-    path('api/hello', views.hello, name='hello'),
+    path('admin/', admin.site.urls),
+    path('api/hello', views.hello, name='hello')
 ]
+
